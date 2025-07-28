6578f16b864c4eaa16e090bb8b00e69606391aea
Omprasad72096 <omprasad72096@gmail.com>
2025-07-28 20:21:04 +0530

    initial commit

---
+++ country.js
@@ -0,0 +1,119 @@
+const cca3 = new URLSearchParams(location.search).get("alpha")
+let url = `https://restcountries.com/v3.1/alpha/${cca3}`;
+let urlBorder = `https://restcountries.com/v3.1/alpha/`
+
+const flagImg = document.querySelector(".flag-img")
+const countryName = document.querySelector(".country-name")
+const nativeName = document.querySelector("#nativeName")
+const popu = document.querySelector("#popu")
+const region = document.querySelector("#reg")
+const subRegion = document.querySelector("#sub-reg")
+const capital = document.querySelector("#cap")
+const tld = document.querySelector("#tld")
+const currencies = document.querySelector("#currencies")
+const lang = document.querySelector("#lang")
+const borderCounty = document.querySelector('.border-country')
+
+const body = document.querySelector('body')
+const darkMode = document.querySelector("#dark")
+
+const back = document.querySelector(".back")
+back.addEventListener("click", () => history.back());
+
+async function fetchAll(borders) {
+    for(const br of borders) {
+        const res = await axios.get(urlBorder+br)
+        const [data] = res.data
+        const ancor = document.createElement("a");
+        ancor.innerHTML = `<a href="country.html?alpha=${data.cca3}" class="button">${data.name.common}</a>`
+        borderCounty.append(ancor)
+    }
+}
+async function showCountry() {
+    let res = await axios.get(url)
+    let [data] = res.data;
+
+    console.log(data)
+    flagImg.src =  data.flags.svg
+    countryName.innerText = data.name.common
+    nativeName.innerText =  data.name.nativeName ? Object.values(data.name.nativeName)[0]?.common : "none";
+    popu.innerText = data.population.toLocaleString('en-IN')
+    region.innerText = data.region
+    subRegion.innerText = data.subregion ? data.subregion : "none";
+    capital.innerText = data.capital ? data.capital.join(", ") : "none";
+    
+    tld.innerText = data.tld.join(", ")
+    currencies.innerText = data.currencies ? Object.values(data.currencies).map((curr) => curr.name).join(", ") : "none";
+    lang.innerText = data.languages ? Object.values(data.languages).join(", ") : ["none"]
+
+    let borders = data.borders ? data.borders : ["none"]
+    if(borders[0] != 'none') {
+        await fetchAll(borders);
+    }
+    else {
+        let p = document.createElement("p")
+        p.innerHTML = `<p>&nbsp;None</p>`
+        borderCounty.append(p)
+    }
+}
+
+darkMode.addEventListener("click", (event) => {
+    body.classList.toggle("dark")
+})
+
+showCountry();
+
+
+//Object.values(data.name.nativeName) -> convert the objects in nativeName to array [{},  {}, {}]
+// "name": {
+//     "common": "India",
+//     "official": "Republic of India",
+//     "nativeName": {
+//         "eng": {
+//             "official": "Republic of India",
+//             "common": "India"
+//         },
+//         "hin": {
+//             "official": "भारत गणराज्य",
+//             "common": "भारत"
+//         },
+//         "tam": {
+//             "official": "இந்தியக் குடியரசு",
+//             "common": "இந்தியா"
+//         }
+//     }
+// },
+
+
+
+//Object.values(data.currencies).map((curr) => curr.name) - convert the data currencies in array, then in that array i seletes the name key and return array of that
+// Object.values(data.currencies) --> [{}, {}]
+
+// map exchange curr obj {} with curr.name
+// Object.values(data.currencies).map((curr) => curr.name) --> ["Indian rupee", "Indian rupee 2"]
+// "currencies": {
+//     "INR": {
+//         "symbol": "₹",
+//         "name": "Indian rupee"
+//     }
+//     "INR 2": {
+//         "symbol": "₹ 2",
+//         "name": "Indian rupee 2"
+//     }
+// },
+
+
+
+
+// let border = ["France", "India", "Nepal"];
+// let borderContainer = document.querySelector(".border-country");
+// borderContainer.innerHTML = `<p><b>Border Countries: </b></p>` +
+// border.map(country => `<a href="#" class="button">${country}</a>`).join("");
+
+// border.map(...) creates an array like:
+
+// [
+//   '<a href="#" class="button">France</a>',
+//   '<a href="#" class="button">India</a>',
+//   '<a href="#" class="button">Nepal</a>'
+// ]
---
+++ script.js
@@ -0,0 +1,95 @@
+// to get india only - https://restcountries.com/v3.1/name/Republic%20of%20India
+//all indipendent country - https://restcountries.com/v3.1/independent
+// all country name and flag - https://restcountries.com/v3.1/all?fields=name,flags
+
+let url = "https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital,cca3"
+const cardContainer = document.querySelector(".country-container")
+
+const filter = document.querySelector("#filter");
+const search = document.querySelector('#inp')
+const body = document.querySelector('body')
+const darkMode = document.querySelector("#dark")
+
+let isFirst = true
+let allCountryData;
+
+async function allCountry() {
+    let res = await axios.get(url);
+    let data = res.data
+    if(isFirst) {
+        allCountryData = data;
+        isFirst = false;
+    }
+    console.log(data)
+    makeAllCards(data);
+}
+function makeAllCards(data){
+    data.forEach((country) => {
+        let cca3 = country.cca3;
+        let flagSvg = country.flags.svg;
+        let countryName = country.name.common;
+        let population = country.population.toLocaleString('en-IN')
+        let region = country.region;
+        let capital = country.capital ? country.capital.join(", ") : "none"
+        makeNewCard(cca3, flagSvg, countryName, population, region, capital)
+    })
+}
+function makeNewCard(cca3, flagSvg ,countryName, population, region, capital) {
+    const countryCard = document.createElement("a")
+    countryCard.classList.add("country-card")
+    countryCard.href = `country.html?alpha=${cca3}`
+
+    countryCard.innerHTML = 
+    `
+        <img src=${flagSvg} alt="flag">
+        <div class="country-info">
+        <h3><b>${countryName}</b></h3>
+        <div class="country-details">
+            <p><b>Population: </b>${population}</p>
+            <p><b>Region: </b>${region}</p>
+            <p><b>Capital: </b>${capital}</p>
+        </div>
+        </div>
+    `
+    cardContainer.appendChild(countryCard)
+}
+
+filter.addEventListener("change" , (event) => {
+    url = `https://restcountries.com/v3.1/region/${filter.value}`
+    cardContainer.innerHTML = ``
+    allCountry()
+})
+search.addEventListener("input", function(event) {
+    let userInp = event.target.value.trim()
+    console.log(event.target.value)
+
+    if(userInp != '') {
+        let searchCountry = [];
+        for(data of allCountryData){
+            let currCountryName = data.name.common
+            if(isSubsequence(userInp, currCountryName)){
+                searchCountry.push(data)
+            }
+        }
+        cardContainer.innerHTML = ``
+        makeAllCards(searchCountry)
+    }
+})
+darkMode.addEventListener("click", (event) => {
+    body.classList.toggle("dark")
+})
+
+function isSubsequence(small, big){
+    small = small.toLowerCase()
+    big = big.toLowerCase()
+    let i = 0, j = 0;
+    while(i<small.length && j<big.length){
+        if(small[i] == big[j]) i++;
+        j++;
+    }
+    return i == small.length
+}
+const isSubString = (small, big) => big.toLowerCase().includes(small.toLowerCase())
+
+
+allCountry();
